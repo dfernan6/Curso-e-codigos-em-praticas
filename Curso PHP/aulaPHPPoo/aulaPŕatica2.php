a6c901e53622b0993fad13db18fc70b774e2b79e
dfernan6 <danilo.afernandes@outlook.com>
2023-02-06 20:56:26 -0300

    Enviando correção nas funções

--- Curso PHP/aulaPHPPoo/aulaPŕatica2.php
+++ Curso PHP/aulaPHPPoo/aulaPŕatica2.php
@@ -1,17 +1,10 @@
 <?php
  require_once "Caneta.php";
  $c1 = new Caneta;
- $c1 ->cor = "Azul";
- $c1 ->ponta = 0.5;
- $c1 ->tampar();
-print_r($c1);
-
-echo "<br>";
-
-  $c2 = new Caneta;
-  $c2 ->cor = "Verde";
-  $c2 ->ponta = 50;
-  $c2 ->tampar();
-print_r($c2);
+ $c1->cor = "Azul";
+ $c1->ponta = 0.5;
+ $c1->destampar();
+ $c1->rabiscar();
+  print_r($c1);
 
 ?>
