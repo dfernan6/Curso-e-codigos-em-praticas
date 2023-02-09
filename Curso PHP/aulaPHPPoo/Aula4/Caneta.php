8176313248cf621f921e8cbecb529c15984b8acf
dfernan6 <danilo.afernandes@outlook.com>
2023-02-09 01:27:32 -0300

    Enviando outro conteúdo dos exercicios da aula 4

--- Curso PHP/aulaPHPPoo/Aula4/Caneta.php
+++ Curso PHP/aulaPHPPoo/Aula4/Caneta.php
@@ -15,21 +15,5 @@ class Caneta {
     public function tampar(){
         $this->tampada = true;
     }
-
-    public function getModelo() {
-        return $this->modelo;
-}
-
-    public function setModelo($m) {
-        $this->modelo = $m;
-        }
-
-    public function getPonta() {
-        return $this->$ponta;
-    }
-
-    public function setPonta($p) {
-        $this->ponta = $p;
-    }
 }
 ?>
---
+++ Curso PHP/aulaPHPPoo/Aula4/aulapratica4-2.php
@@ -0,0 +1,8 @@
+<pre>
+<?php
+ require_once "Quarto-1.php";
+ $c1 = New Quarto("Amarelo", 25);
+
+ print_r($c1);
+?>
+</pre>
