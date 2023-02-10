b02816865bd94fa9fec03f7513e1664affcff095
dfernan6 <danilo.afernandes@outlook.com>
2023-02-10 00:14:45 -0300

    Enviando os exercicios em Getter e Setter aula5

--- Curso PHP/aulaPHPPoo/ContaBanco-1.php
+++ Curso PHP/aulaPHPPoo/ContaBanco-1.php
@@ -1,3 +1,72 @@
 <?php
+  class ContaBanco {
+    public $numConta;
+    protected $tipo;
+    private $dono;
+    private $saldo;
+    private $status;
 
+    public function getNumConta(){
+        return $this->numConta;
+    }
+
+    public function setNumConta($n){
+        $this->numConta = $n;
+    }
+
+    public function getTipo(){
+        return $this->tipo;
+    }
+
+    public function setTipo($t){
+        $this->tipo = $t;
+
+    }
+
+    public function getDono(){
+        return $this->dono;
+    }
+
+    public function setDono($d){
+        $this->dono = $d;
+    }
+
+    public function getSaldo(){
+        return $this->saldo;
+    }
+
+    public function setSaldo($s){
+        $this->saldo = $s;
+    }
+
+    public function getStatus(){
+        return $this->status;
+    }
+
+    public function setStatus($st){
+        $this->status = $st;
+    }
+
+    public function abrirConta(){
+        $this->status = true; 
+}
+    public function fecharConta(){
+        if($this->saldo <= 0);
+        $this->status = false;
+    }
+
+    public function depositar($s){
+        $this->saldo += $s;
+
+    }
+
+    public function sacar($s){
+        $this->saldo -= $s;
+    }
+
+    public function pagarMensal($s){
+        $this->saldo -= $s;
+    }
+
+  }
 ?>
--- Curso PHP/aulaPHPPoo/aulaPratica5-1.php
+++ Curso PHP/aulaPHPPoo/aulaPratica5-1.php
@@ -1,3 +1,31 @@
+<pre>
 <?php
+require_once "ContaBanco-1.php";
+$alberto = New ContaBanco();
+$alberto->setNumConta(2202);
+$alberto->setTipo("cC");
+$alberto->setDono("Alberto Genuino Silva");
+$alberto->setSaldo(50);
+$alberto->setStatus(true);
+//$alberto->abrirConta();
+$alberto->fecharConta();
+$alberto->depositar(10);
+$alberto->sacar(60);
+$alberto->pagarMensal(0);
 
+$jaque = New ContaBanco();
+$jaque->setNumConta(3210);
+$jaque->setTipo("cP");
+$jaque->setDono("Jaqueline Gonçalves cunha");
+$jaque->setSaldo(150);
+$jaque->setStatus(true);
+$jaque->abrirConta();
+//$jaque->fecharConta();
+$jaque->depositar(0);
+$jaque->sacar(0);
+$jaque->pagarMensal(0);
+
+print_r($alberto);
+print_r($jaque)
 ?>
+</pre>
