8e54e8029af36e599e2475298c53c3810b910917
dfernan6 <danilo.afernandes@outlook.com>
2023-02-10 00:45:13 -0300

    Enviando o exercicios Construct da aula5

--- Curso PHP/aulaPHPPoo/Aula5/aulaPratica5.php
+++ Curso PHP/aulaPHPPoo/Aula5/aulaPratica5.php
@@ -1,3 +1,21 @@
+<pre>
 <?php
+require_once "ContaBanco.php";
+$alberto = New ContaBanco(2202, "cC", "Alberto Genuino Silva", 0, true);
+//$alberto->abrirConta();
+$alberto->fecharConta();
+$alberto->depositar(5);
+$alberto->sacar(0);
+$alberto->pagarMensal(5);
 
+$jaque = New ContaBanco(3210, "cP", "Jaqueline Gonçalves cunha", 150, true);
+$jaque->abrirConta();
+//$jaque->fecharConta();
+$jaque->depositar(50);
+$jaque->sacar(10);
+$jaque->pagarMensal(30);
+
+print_r($alberto);
+print_r($jaque)
 ?>
+</pre>
