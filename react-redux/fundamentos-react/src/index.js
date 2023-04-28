243d4f503773007670881598ab164688301ab589
dfernan6 <danilo.afernandes@outlook.com>
2023-04-28 12:41:48 -0300

    Enviando mais conteudo de propriedades JSX

--- react-redux/fundamentos-react/src/index.js
+++ react-redux/fundamentos-react/src/index.js
@@ -2,13 +2,28 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 const tag = <strong>'Olá React!!'</strong>
 
 ReactDOM.render(
-    <div>
+    <div id="app">
      { tag }
-     <p><Primeiro></Primeiro></p>
+     <Primeiro />
+     <ComParametro
+       titulo="Situação do aluno"
+       aluno="Pedro"
+       nota=" 9.5" />
+       <ComParametro
+       titulo="Situação do aluno"
+       aluno="Maria"
+       nota=" 7" 
+       />
+              <ComParametro
+       titulo="Situação do aluno"
+       aluno="Lucia"
+       nota=" 6.3" 
+       />
     </div>,
     document.getElementById('root')
 )
