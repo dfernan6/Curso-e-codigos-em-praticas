ef21302cef2d9775677b2e251952a70f5d3bb63c
dfernan6 <danilo.afernandes@outlook.com>
2023-04-28 13:57:34 -0300

    Enviando arquivos com App JSX

--- react-redux/fundamentos-react/src/index.js
+++ react-redux/fundamentos-react/src/index.js
@@ -1,29 +1,9 @@
 import './index.css'
-import ReactDOM from 'react-dom'
 import React from 'react'
-import Primeiro from './components/basicos/Primeiro'
-import ComParametro from './components/basicos/ComParametro'
-
-const tag = <strong>'Olá React!!'</strong>
+import ReactDOM from 'react-dom'
+import App from './App'
 
 ReactDOM.render(
-    <div id="app">
-     { tag }
-     <Primeiro />
-     <ComParametro
-       titulo="Situação do aluno"
-       aluno="Pedro"
-       nota=" 9.5" />
-       <ComParametro
-       titulo="Situação do aluno"
-       aluno="Maria"
-       nota=" 7" 
-       />
-              <ComParametro
-       titulo="Situação do aluno"
-       aluno="Lucia"
-       nota=" 6.3" 
-       />
-    </div>,
+    <App />,
     document.getElementById('root')
 )
