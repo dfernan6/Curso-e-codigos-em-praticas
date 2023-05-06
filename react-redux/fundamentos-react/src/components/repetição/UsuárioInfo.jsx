9e9bed02e66107587b7907546492df2a4a54fbeb
dfernan6 <danilo.afernandes@outlook.com>
2023-05-06 12:59:14 -0300

    Finalizando renderização condicional

--- react-redux/fundamentos-react/src/components/repetição/UsuárioInfo.jsx
+++ react-redux/fundamentos-react/src/components/repetição/UsuárioInfo.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-import If from "../../data/If";
+import If, { Else } from "../../data/If";
 
 export default (props) => {
     const usuario = props.usuario || {};
     return (
         <div>
-            <If test={usuario && usuario.nome}>
+           {/* <If test={usuario && usuario.nome}>
                 Seja Bem vindo <strong>{usuario.nome}</strong>
             </If>
             <If test={!usuario || !usuario.nome}>
                 Seja Bem vindo <strong>Amigão</strong>
+    </If> */}
+            <If test={usuario && usuario.nome}>
+                Seja Bem vindo <strong>{usuario.nome}</strong>
+           
+            <Else>
+            Seja Bem vindo <strong>Amigão</strong>
+            </Else>
             </If>
         </div>
     )
