b79a7e1130b7c6dca8b13f74307e095146915e63
unknown <danilo.afernandes@outlook.com>
2024-02-16 14:31:31 -0300

    Alterando configurações necessárias

--- Curso JS/AluraAPINodes/server.js
+++ Curso JS/AluraAPINodes/server.js
@@ -1,18 +1,7 @@
-//import http from "http";
-import app from "./src/app.js";
+import "dotenv/config"
+import app from "./src/app.js"; 
 
 const PORT = 3000;
-
-const rotas = {
-    "/": "Curso de NodeJs.", 
-    "/livros": "Entrei na rota livros.",
-    "/autores": "Entrei na rota autores."
-}
-
-//const server = http.createServer((req, res) => {
- //   res.writeHead(200, {"content-type": "text/plain"});
-  //  res.end(rotas[req.url]);
-//});
 
 app.listen(PORT, () => {
     console.log("Executing now...")
--- Curso JS/AluraAPINodes/src/app.js
+++ Curso JS/AluraAPINodes/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
-import connectToDatabase from "./dbConnect.js";
+import connectToDatabase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
 
 const conexao = await connectToDatabase();
 
@@ -14,44 +15,29 @@ conexao.once("open", () => {
 const app = express();
 app.use(express.json())
 
-function buscaLivros(id){
-    return livros.findIndex(livro => {
-        return livro.id === Number(id)
-    })
-}
-
-const livros = [{
-    id: 1,
-    titulo: "O senhor dos anéis"
-},{
-    id: 2,
-    titulo: "O Hobbit"
-}]
-
 app.get("/", (req, res) => {
     res.status(200).send("Curso de NodeJS.");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json({livros});
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
+
 });
 
 app.get("/livros/:id", (req, res) => {
     const index = buscaLivros(req.params.id);
     res.status(200).json(livros[index]);
 });
-
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("Cadastrado com sucesso!");
 });
-
 app.put("/livros/:id", (req, res) => {
     const index = buscaLivros(req.params.id);
     livros[index].titulo = req.body.titulo;
     res.status(200).json(livros);
 });
-
 app.delete("/livros/:id", (req, res) => {
     const index = buscaLivros(req.params.id);
     livros.splice(index, 1);
