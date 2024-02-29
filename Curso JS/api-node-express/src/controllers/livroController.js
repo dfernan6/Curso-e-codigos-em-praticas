523801ba996a294002eba91cbc2342eed7b88789
unknown <danilo.afernandes@outlook.com>
2024-02-29 08:07:15 -0300

    Finalizando Node.JS: Lidando com buscas, filtros, paginação e erros em uma API

--- Curso JS/api-node-express/src/controllers/livroController.js
+++ Curso JS/api-node-express/src/controllers/livroController.js
@@ -1,15 +1,15 @@
 import NaoEncontrado from "../erros/NaoEncontrado.js";
-import { livros } from "../models/index.js";
+import { autores, livros } from "../models/index.js";
 
 class LivroController {
 
   static listarLivros = async (req, res, next) => {
     try {
-      const livrosResultado = await livros.find()
-        .populate("autor")
-        .exec();
+      const buscaLivros = livros.find();
 
-      res.status(200).json(livrosResultado);
+      req.resultado = buscaLivros;
+
+      next();
     } catch (erro) {
       next(erro);
     }
@@ -19,14 +19,14 @@ class LivroController {
     try {
       const id = req.params.id;
 
-      const livroResultados = await livros.findById(id)
+      const livroResultado = await livros.findById(id)
         .populate("autor", "nome")
         .exec();
 
-      if (livroResultados !== null) {
-        res.status(200).send(livroResultados);
+      if (livroResultado !== null) {
+        res.status(200).send(livroResultado);
       } else {
-        next(new NaoEncontrado("Id do Livro não localizado."));
+        next(new NaoEncontrado("Id do livro não localizado."));
       }
     } catch (erro) {
       next(erro);
@@ -49,11 +49,12 @@ class LivroController {
     try {
       const id = req.params.id;
     
-      const livroAtualizar = await livros.findByIdAndUpdate(id, {$set: req.body});
-      if (livroAtualizar !== null) {
+      const livroResultado = await livros.findByIdAndUpdate(id, {$set: req.body});
+
+      if (livroResultado !== null) {
         res.status(200).send({message: "Livro atualizado com sucesso"});
       } else {
-        next(new NaoEncontrado("Id do Livro não localizado."));
+        next(new NaoEncontrado("Id do livro não localizado."));
       }
     } catch (erro) {
       next(erro);
@@ -64,14 +65,13 @@ class LivroController {
     try {
       const id = req.params.id;
 
-      const livroExcluir = await livros.findByIdAndDelete(id);
+      const livroResultado = await livros.findByIdAndDelete(id);
 
-      if (livroExcluir !== null) {
+      if (livroResultado !== null) {
         res.status(200).send({message: "Livro removido com sucesso"});
       } else {
-        next(new NaoEncontrado("Não encontrado o ID para deletar!"));
+        next(new NaoEncontrado("Id do livro não localizado."));
       }
-
     } catch (erro) {
       next(erro);
     }
@@ -79,31 +79,49 @@ class LivroController {
 
   static listarLivroPorFiltro = async (req, res, next) => {
     try {
-      const busca = processaBusca(req.query);
+      const busca = await processaBusca(req.query);
 
-      const livrosResultado = await livros.find(busca);
+      if (busca !== null) {
+        const livrosResultado = livros
+          .find(busca)
+          .populate("autor");
 
-      res.status(200).send(livrosResultado);
+        req.resultado = livrosResultado;
+
+        next();
+      } else {
+        res.status(200).send([]);
+      }
     } catch (erro) {
       next(erro);
     }
   };
 }
 
-function processaBusca(parametros) {
-  const { editora, titulo, miniPaginas, maxpaginas } = parametros;
+async function processaBusca(parametros) {
+  const { editora, titulo, minPaginas, maxPaginas, nomeAutor } = parametros;
 
-  const busca = {};
+  let busca = {};
 
   if (editora) busca.editora = editora;
   if (titulo) busca.titulo = { $regex: titulo, $options: "i" };
 
-  if (miniPaginas || maxpaginas) busca.numeroPaginas = {};
+  if (minPaginas || maxPaginas) busca.numeroPaginas = {};
 
-  if (miniPaginas) busca.numeroPaginas.$gte = miniPaginas ;
-  if (maxpaginas) busca.numeroPaginas.$lte = maxpaginas;
+  // gte = Greater Than or Equal = Maior ou igual que
+  if (minPaginas) busca.numeroPaginas.$gte = minPaginas;
+  // lte = Less Than or Equal = Menor ou igual que
+  if (maxPaginas) busca.numeroPaginas.$lte = maxPaginas;
 
+  if (nomeAutor) {
+    const autor = await autores.findOne({ nome: nomeAutor });
 
+    if (autor !== null) {
+      busca.autor = autor._id;
+    } else {
+      busca = null;
+    }
+  }
 
   return busca;
 }
