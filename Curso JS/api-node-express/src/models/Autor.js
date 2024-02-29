9fa35487303700e51f9f16f75608273593a9a257
unknown <danilo.afernandes@outlook.com>
2024-02-29 04:56:27 -0300

    Enviando busco por titulo do livro

--- Curso JS/api-node-express/src/models/Autor.js
+++ Curso JS/api-node-express/src/models/Autor.js
@@ -5,7 +5,8 @@ const autorSchema = new mongoose.Schema(
     id: {type: String},
     nome: {
       type: String, 
-      required: [true, "O nome do autor é obrigatório!"]},
+      required: [true, "O nome do autor é obrigatório!"]
+    },
     nacionalidade: {type: String}
   },
   {
