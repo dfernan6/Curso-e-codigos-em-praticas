53bd47157afccfcd6f7fb4471e3b219eececf4f2
unknown <danilo.afernandes@outlook.com>
2024-03-28 04:52:47 -0300

    Adicionando os components redux

--- react-redux/redux-simples/src/components/Media.jsx
+++ react-redux/redux-simples/src/components/Media.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import Card from './Card'
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default props => {
-    
-    const {min, max} = props
-    
+function Media(props) { 
+  const {min, max} = props
     return (
       <Card title="Média dos Números" green>
         <div>
           <span>
             <span>Resultado: </span>
-            <strong>{(max + min) / 2}</strong>  
+            <strong>{(max + min) / 2 }</strong>  
           </span>
         </div>
       </Card>
   )
 }
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max,
+  };
+} 
+
+export default connect(mapStateToProps)(Media)
--- react-redux/redux-simples/src/components/Soma.jsx
+++ react-redux/redux-simples/src/components/Soma.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import Card from './Card'
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default props => {
-
-  const { min, max } = props
-
+function Soma(props) {
+  const {min, max} = props
     return (
         <Card title="Soma dos Números" red>
           <div>
             <span>
               <span>Resultado: </span>
-                <strong>{ min + max }</strong>  
+                <strong>{(min + max)}</strong>  
               </span>
           </div>
         </Card>
     )
 }
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max,
+  };
+}
+
+export default connect(mapStateToProps)(Soma)
