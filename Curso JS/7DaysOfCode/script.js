2c26a42edefb94349337ad92db08b5823972774c
unknown <danilo.afernandes@outlook.com>
2025-07-26 14:35:15 -0300

    Enviado o #7DaysOfCode - Lógica JS 2/7: Variáveis

--- Curso JS/7DaysOfCode/script.js
+++ Curso JS/7DaysOfCode/script.js
@@ -25,3 +25,19 @@ if (numeroDez == stringDez) {
   console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
 }
 
+//#7DaysOfCode - Lógica JS 2/7: 👩🏽‍💻 Variáveis
+
+function cadastro(){
+    var nome = document.getElementById("nome").value;
+    var idade = document.getElementById("idade").value;
+    var linguagem = document.getElementById("linguagem").value;
+ 
+    var mensagem = document.getElementById("seltab");
+    mensagem.innerHTML += `<option>Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!</option>`;
+
+if (prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO?`) == '1') {
+  alert("Muito bom! Continue estudando e você terá muito sucesso.");
+} else {
+  alert("Ahh que pena... Já tentou aprender outras linguagens?");
+    }
+  };
