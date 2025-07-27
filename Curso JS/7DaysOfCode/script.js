8cefcfaecc392b781218d196ed364c060d0cd5b1
unknown <danilo.afernandes@outlook.com>
2025-07-27 14:55:21 -0300

    Finalizando o dia #3 do desafio

--- Curso JS/7DaysOfCode/script.js
+++ Curso JS/7DaysOfCode/script.js
@@ -26,18 +26,61 @@ if (numeroDez == stringDez) {
 }
 
 //#7DaysOfCode - Lógica JS 2/7: 👩🏽‍💻 Variáveis
+let nome;
+let idade;
+let linguagem;
+let mensagem;
 
 function cadastro(){
-    var nome = document.getElementById("nome").value;
-    var idade = document.getElementById("idade").value;
-    var linguagem = document.getElementById("linguagem").value;
- 
-    var mensagem = document.getElementById("seltab");
-    mensagem.innerHTML += `<option>Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!</option>`;
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    let linguagem = document.getElementById("linguagem").value;
+    let mensagem = document.getElementById("seltab");
+    const reply = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO?`)
+    mensagem.innerHTML += `<option>Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!</option>`
 
-if (prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO?`) == '1') {
-  alert("Muito bom! Continue estudando e você terá muito sucesso.");
+if ( reply == '1') {
+ var alert = "Muito bom! Continue estudando e você terá muito sucesso.";
 } else {
-  alert("Ahh que pena... Já tentou aprender outras linguagens?");
+ var alert = "Ahh que pena... Já tentou aprender outras linguagens?";
+  }
+    
+    mensagem.innerHTML += `<option>${alert}</option>`;
+
+     const msg = prompt(`Você que estuda ${linguagem}?  Se você quer seguir para qual área?
+ Front-end  1 ou back-end 2.`)
+
+ if ( msg == '1') {
+  let alert = "font-end";
+  prompt(`Além de seu foco em ${alert} ,qual linguagem você quer aprender? React 1 ou Vue 2`);
+} else {
+  let alert = "back-end";
+  prompt(`Além de seu foco em ${alert} ,qual linguagem você quer aprender? C# 1 ou Java 2`);
+}
+
+    const msg2 = prompt(`E você ${nome} gostaria de seguir se especializando na área escolhida 
+ou seguir se desenvolvendo para se tornar Fullstack?
+ Area escolhida 1 ou Fullstack 2`)
+
+
+ if ( msg2 == '1') {
+ let alert2 = "Area escolhida";
+    prompt( `Quais são as tecnologias essenciais para ${alert2} ?`)
+    for (let i = 0; i < 5; i++) {
+      const msg3 = prompt(`Mais alguma tecnologia?`)
+          mensagem.innerHTML += `${msg3}`
     }
-  };
+} else {
+let alert2 = "Area escolhida";
+    prompt( `Quais são as tecnologias essenciais para ${alert2} ?`)
+    for (let i = 0; i < 5; i++) {
+      const msg3 = prompt(`Mais alguma tecnologia?`)
+          mensagem.innerHTML += `${msg3}`
+    }
+  }
+
+};
+
+
+//#7DaysOfCode - Lógica JS 3/7: Fluxo de decisão
+
