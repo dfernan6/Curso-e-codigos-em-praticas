9fde9f2de8b713d4b0c6d96f6d3a1ac219b82caa
unknown <danilo.afernandes@outlook.com>
2025-08-13 19:50:38 -0300

    Alterando o local correto do display da classe

--- react-fundamentals/src/components/repetição/TabelaProdutos.jsx
+++ react-fundamentals/src/components/repetição/TabelaProdutos.jsx
@@ -9,16 +9,16 @@ export default (props) => {
             <tr key={produtos.nome}>
                 <td>{produtos.id}</td>
                 <td>{produtos.nome}</td>
-                <td>R$ {produtos.preço}</td>
+                <td>R$ {produtos.preço}</td> 
             </tr>
         )
     });
     return (
         <div className="TabelaProduto">
             <table border={1} >
-            <th>Preço</th>
             <th>ID</th>
-            <th>Nome</th> 
+            <th>Nome</th>
+            <th>Preço</th> 
              {produtosLi}
             </table>
         </div>
